adc7bc44c82521db13d362a74d9c29382c376f5a
ajnauleau <ajnauleau@gmail.com>
2018-03-20 20:01:42 +0100

    Network Aspect 2.1 done

--- Graphic/client.py
+++ Graphic/client.py
@@ -17,28 +17,3 @@ def getresult(value):
 
         print('\tData from server : {}'.format(result))
         return result.decode()
-
-class Handler:
-
-    def __init__(self, builder):
-
-        self.builder = builder
-
-    def on_principle_window_delete_event(self, widget, event):
-
-        print("Bye.")
-        Gtk.main_quit(widget, event)
-
-    def on_validation_button_clicked(self, button):
-
-        print("Hello World!")
-        entry = self.builder.get_object('entry')
-        result = self.builder.get_object('result')
-        value = entry.get_text()
-
-        if value:
-            result.set_text(getresult(value))
-        else:
-            result.set_text('')
-
-
--- Graphic/ihm.py
+++ Graphic/ihm.py
@@ -6,24 +6,28 @@ from gi.repository import Gtk
 class Handler:
 
     def __init__(self, builder):
+
         self.builder = builder
 
     def on_principle_window_delete_event(self, widget, event):
+
         print("Bye.")
         Gtk.main_quit(widget, event)
 
     def on_validation_button_clicked(self, button):
-        print ("Hello World!")
+
+        print("Hello World!")
         entry = self.builder.get_object('entry')
         result = self.builder.get_object('result')
         value = entry.get_text()
+
         if value:
             result.set_text(value)
         else:
             result.set_text('')
 
 builder = Gtk.Builder()
-builder.add_from_file("hello.glade")
+builder.add_from_file("/Users/Antoine/Developer/Code/Python/Glade/hello.glade")
 builder.connect_signals(Handler(builder))
 
 if __name__ == "__main__":
