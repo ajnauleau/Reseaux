5f9b7058cfa2f9dd3ecb99d722b69e73f434d94a
ajnauleau <ajnauleau@gmail.com>
2018-03-23 15:54:34 +0100

    Network Aspect 2.2 done *split client and hello ihm

---
+++ Graphic/ihm_client.py
@@ -0,0 +1,51 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class Handler:
+
+    def __init__(self, builder):
+
+        self.builder = builder
+
+    def on_principle_window_delete_event(self, widget, event):
+
+        print("Bye.")
+        Gtk.main_quit(widget, event)
+
+    def on_hello_button_clicked(self, button):
+
+        print("Hello World!")
+        entry = self.builder.get_object('entry')
+        result = self.builder.get_object('result')
+        value = entry.get_text()
+
+        if value:
+            result.set_text("hello", value)
+        else:
+            result.set_text('')
+
+    def on_bye_button_clicked(self, button):
+
+        print("Bye World!")
+        entry = self.builder.get_object('entry')
+        result = self.builder.get_object('result')
+        value = entry.get_text()
+
+        if value:
+            result.set_text("bye", value)
+        else:
+            result.set_text('')
+
+builder = Gtk.Builder()
+builder.add_from_file("/Users/Antoine/Developer/Code/Python/Glade/client.glade")
+builder.connect_signals(Handler(builder))
+
+if __name__ == "__main__":
+    window = builder.get_object("principle_window")
+    window.show_all()
+
+    Gtk.main()
+
+
