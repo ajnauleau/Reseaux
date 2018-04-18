6b24aba61e65c11918a40ac6f0e3024ff9da38bf
ajnauleau <ajnauleau@gmail.com>
2018-04-18 21:56:37 +0200

    finished OpenVPN class, tested.

---
+++ Scapy/openvpn.py
@@ -0,0 +1,19 @@
+
+from scapy.all import Packet, BitEnumField, BitField
+
+class OpenVPN(Packet):
+    name = 'OpenVPN'
+    fields_desc = [
+        BitEnumField('opcode', 1, 5, {
+            1: 'P_CONTROL_HARD_RESET_CLIENT_V1',
+            2: 'P_CONTROL_HARD_RESET_SERVER_V1',
+            3: 'P_CONTROL_SOFT_RESET_V1',
+            4: 'P_CONTROL_V1',
+            5: 'P_ACK_V1',
+            6: 'P_DATA_V1',
+            7: 'P_CONTROL_HARD_RESET_CLIENT_V2',
+            8: 'P_CONTROL_HARD_RESET_SERVER_V2',
+            9: 'P_DATA_V2',
+        }),
+        BitField('keyid', 0, 3),
+    ]
